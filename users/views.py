a9057a306bd8dfb7e5733b27f3667c03a250231f
Oznur Celebi <o.ozgumus17@gmail.com>
2022-03-10 11:42:58 +0100

    user views.py added

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import Userfrom
+
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+
+
+#Serializeri olusturdduk simdi views i yaziyoruz
+
+class RegisterAPI(CreateAPIView):
+    queryset =User.objects.all()
+    serializer_class =RegisterSerializer
 
 # Create your views here.
